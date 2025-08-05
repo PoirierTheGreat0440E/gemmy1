e7e7510833bbd7fcdfe8a6e8c46e1dc238b0241a
Alexandre Baron <poirierantoine52@gmail.com>
2025-08-05 13:37:49 +0200

    GUI Client +

--- socket_client.py
+++ socket_client.py
@@ -19,10 +19,6 @@ class Shower(OpenGLFrame):
     def redraw(self):
         pass
 
-def tentative_connexion(surnom,adresse):
-    print("Surnom : " + surnom)
-    print("Adresse : " + adresse)
-
 def quitter_application(event):
     root.destroy()
 
@@ -52,11 +48,12 @@ class FenetreConnexion(tkinter.Toplevel):
         self.texte_adresse.grid(row=1,column=1,padx=20,pady=20)
 
         self.bouton_connexion = tkinter.Button(self,text="Connexion")
-        self.bouton_connexion.grid(row=2,column=0,columnspan=2,padx=20,pady=20)
+        self.bouton_connexion.grid(row=2,column=0,padx=20,pady=20)
         self.bouton_connexion.bind('<Button>',self.tenter_connexion)
 
-        # Le connecteur qui sera utilisé pour communiquer avec un serveur.
-        self.connecteur = socket.socket()
+        self.bouton_quitter = tkinter.Button(self,text="Annuler", command = lambda : self.destroy() )
+        self.bouton_quitter.grid(row=2,column=1,padx=20,pady=20)
+        
 
     def tenter_connexion(self,event):
         SURNOM = "GUEST"
@@ -66,26 +63,9 @@ class FenetreConnexion(tkinter.Toplevel):
         if  self.adresse_saisie.get().strip() != "":
             ADRESSE = adresse_saisie.get()
         PORT = 65000
-        print("Connection vers {}:{}".format(ADRESSE,PORT))
-        try:
-
-            connexion = self.connecteur.connect( (ADRESSE,PORT)  )
-            print("Connexion créée avec succès")
-            reception = ""
-            while reception != "fincommu" :
- 
-                envoi = input(">>> : ")
-                self.connecteur.sendall( bytearray(envoi+"\n","utf-8")  )              
-
-                reception = self.connecteur.recv(1024).decode("utf-8").strip()
-                print(reception)
-                           
-            self.connecteur.close()
-            print("Connexion fermée avec succès")
-        except Exception as erreur:
-            print("Connexion échouée !")
-            print(erreur)
-
+        self.bouton_quitter.invoke()
+        self.master.communication(SURNOM,ADRESSE,PORT)
+        
 
 class FenetrePrincipale(tkinter.Frame):
 
@@ -99,8 +79,8 @@ class FenetrePrincipale(tkinter.Frame):
         self.SHOWER1.configure(width=400,height=500)
         self.SHOWER1.animate = 10
         self.SHOWER1.pack(expand=True,fill=tkinter.BOTH,side=tkinter.LEFT)
-
-
+        
+       
 class TableauDeBord(tkinter.Frame):
 
     def __init__(self,master):
@@ -116,17 +96,44 @@ class TableauDeBord(tkinter.Frame):
         self.bouton_quitter.grid(row=1,column=0,ipadx=5,ipady=5,padx=10,pady=10)
         self.bouton_quitter.bind('<Button>',quitter_application)
 
-        self.label_position_surnom = tkinter.Label(self,text="Surnom : ???")
-        self.label_position_surnom.grid(row=2,column=0,ipady=5,padx=10,pady=10)
+        self.label_surnom = tkinter.Label(self,text="Surnom : ???")
+        self.label_surnom.grid(row=2,column=0,ipady=5,padx=10,pady=10)
 
         self.label_position_x = tkinter.Label(self,text="Position X : ???")
         self.label_position_x.grid(row=3,column=0,ipady=5,padx=10,pady=10)
 
         self.label_position_z = tkinter.Label(self,text="Position Z : ???")
         self.label_position_z.grid(row=4,column=0,ipady=5,padx=10,pady=10)
+        
+        # Le connecteur qui sera utilisé pour communiquer avec un serveur.
+        self.connecteur = socket.socket()
 
     def tenter_une_connexion(self,event):
-        FC1 = FenetreConnexion(root) 
+        FC1 = FenetreConnexion(self) 
+
+    def communication(self,SURNOM,ADRESSE,PORT):
+        print("Connection vers {}:{}".format(ADRESSE,PORT))
+        try:
+
+            connexion = self.connecteur.connect( (ADRESSE,PORT)  )
+            print("Connexion créée avec succès")
+            self.label_surnom.config(text="Surnom : "+SURNOM)
+            self.label_position_x.config(text="Position X : 000")
+            self.label_position_z.config(text="Position Z : 000")
+            reception = ""
+            while reception != "fincommu" :
+ 
+                envoi = input(">>> : ")
+                self.connecteur.sendall( bytearray(envoi+"\n","utf-8")  )              
+
+                reception = self.connecteur.recv(1024).decode("utf-8").strip()
+                print(reception)
+                           
+            self.connecteur.close()
+            print("Connexion fermée avec succès")
+        except Exception as erreur:
+            print("Connexion échouée !")
+            print(erreur)
 
 def main():
    
