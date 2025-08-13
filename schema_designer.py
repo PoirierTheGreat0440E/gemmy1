41195301cf4ca72795ca470e74c5eecaa51cf4ce
PoirierTheGreat0440E <poirierantoine52@gmail.com>
2025-08-13 11:35:47 +0200

    Schema+

--- schema_designer.py
+++ schema_designer.py
@@ -17,6 +17,17 @@ class Liste_des_vertex(tkinter.Frame):
         self.grid(row=0,column=2)
         self.liste_conteneur = tkinter.Listbox(self,bg="magenta",height=25,width=37)
         self.liste_conteneur.pack(side=tkinter.TOP)
+        self.liste_de_vertex = []
+
+    def mettre_listbox_a_jour(self):
+        self.liste_conteneur.delete(0,tkinter.END)
+        for vertex in self.liste_de_vertex:
+            self.liste_conteneur.insert(tkinter.END,vertex)
+
+    def inserer_vertex(self,vertex_insertion):
+        self.liste_de_vertex.append(vertex_insertion.position_array())
+        self.mettre_listbox_a_jour()
+        print("Insertion vertex reussie!!!")
 
     
 class Tableau_de_bord(tkinter.Frame):
@@ -25,6 +36,8 @@ class Tableau_de_bord(tkinter.Frame):
         super().__init__(master)
         self.configure(width=int(LONGUEUR/4),height=HAUTEUR,bg="yellow")
         self.grid(row=0,column=0)
+        self.label_mode = tkinter.Label(self,text="MODE:???")
+        self.label_mode.pack()
 
 class Visionneur_Tridimensionnel(OpenGLFrame):
 
@@ -52,23 +65,37 @@ class FenetrePrincipale(tkinter.Frame):
         self.VT1 = Visionneur_Tridimensionnel(self)
         self.LDV1 = Liste_des_vertex(self)
         self.mode = None # Le mode de la fenetre...
+        self.vertex_curseur = vertexus.vertex_specification(0,0,0)
 
     def reaction_clavier_relache(self,event):
-        if event.keysym == 'i':
+        if event.keysym == 'q':
             self.mode = "INSERTION"
-        elif event.keysym == 'v':
+        elif event.keysym == 'w':
             self.mode = "VISUAL"
-        elif event.keysym == "Escape":
-            self.mode = None
-        #self.mise_a_jour_label_mode()
+        self.TDB1.label_mode.configure(text="MODE:"+self.mode)
 
     def reaction_clavier_appui(self,event):
+        print(event)
         if self.mode == "INSERTION":
-            pass
-        elif self.mode == "VISUAL":
-            pass
+            if event.keysym == "space":
+                self.LDV1.inserer_vertex(self.vertex_curseur)
         # Dans tous les cas, on peut déplacer le curseur...
-        # deplacer_curseur...
+        # Changement de position selon l'axe X
+        if event.keysym == 'u':
+            self.vertex_curseur.nouvelle_position(self.vertex_curseur.pos_x+1,self.vertex_curseur.pos_y,self.vertex_curseur.pos_z)
+        elif event.keysym == 'j':
+            self.vertex_curseur.nouvelle_position(self.vertex_curseur.pos_x-1,self.vertex_curseur.pos_y,self.vertex_curseur.pos_z)
+        # Changement de position selon l'axe Y
+        elif event.keysym == 'i':
+            self.vertex_curseur.nouvelle_position(self.vertex_curseur.pos_x,self.vertex_curseur.pos_y+1,self.vertex_curseur.pos_z)
+        elif event.keysym == 'k':
+            self.vertex_curseur.nouvelle_position(self.vertex_curseur.pos_x,self.vertex_curseur.pos_y-1,self.vertex_curseur.pos_z)
+        # Changement de position selon l'axe Z
+        elif event.keysym == 'o':
+            self.vertex_curseur.nouvelle_position(self.vertex_curseur.pos_x,self.vertex_curseur.pos_y,self.vertex_curseur.pos_z+1)
+        elif event.keysym == 'l':
+            self.vertex_curseur.nouvelle_position(self.vertex_curseur.pos_x,self.vertex_curseur.pos_y,self.vertex_curseur.pos_z-1)
+
 
 def main():
     root = tkinter.Tk()
